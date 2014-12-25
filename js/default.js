37d0b8ac1ed1e486f4f2bb07c4918d1ab0579b99
Scott Byrns <contact@scottbyrns.com>
2014-12-24 21:59:51 -0700

    holoide commit

--- js/default.js
+++ js/default.js
@@ -3,24 +3,7 @@ var windowWidth = window.innerWidth,
 var camera, renderer, scene;
 var group;
 var clock = new THREE.Clock();
-var f = "helvetiker";
-var lf1 = f;
-var lf2 = f;
-var lf3 = f;
-var g_hour = 0;
-var g_minute = 0;
-var g_AMPM = "AM";
-var material = new THREE.MeshFaceMaterial([
-    new THREE.MeshPhongMaterial({
-        color: 0xffffff,
-        shading: THREE.FlatShading
-    }), // front
-    new THREE.MeshPhongMaterial({
-        color: 0xffffff,
-        shading: THREE.SmoothShading
-    }) // side
-]);
-var MeshHH, GeoMM, GeoAM;
+
 var bInitTimeObject = false;
 window.onload = function() {
     console.log("onload");
@@ -30,6 +13,10 @@ window.onload = function() {
 
 
 var uniforms;
+
+
+function Init() {
+
     uniforms = {
         time: {
             type: "f",
@@ -38,22 +25,9 @@ var uniforms;
         surface: {
             type: "t",
             value: new THREE.ImageUtils.loadTexture('resource/sun.jpg')
-        }}; // regular texture
-
-function Init() {
-
-//     uniforms = {
-//         time: {
-//             type: "f",
-//             value: 1.0
-//         },
-//         surface: {
-//             type: "t",
-//             value: new THREE.ImageUtils.loadTexture('resource/brickwall_900x600_small.jpg')
-//         } // regular texture
-
-//     };
-
+        }
+    };
+  
     scene = new THREE.Scene();
     group = new THREE.Object3D();
 
@@ -91,20 +65,8 @@ function animate() {
 
     uniforms.time.value += 0.2 * delta;
 
-//     var date = new Date(Date.now());
-//     var minute = date.getMinutes(); //date.getSeconds();
-//     var hour = date.getHours();
-//     if (g_hour != hour) {
-//         g_hour = hour;
-//         UpateTimeObject();
-//     }
-//     if (g_minute != minute) {
-//         g_minute = minute;
-//         UpateTimeObject();
-//     }
-
-//     group.rotation.z = 0.6 * 0.6 * Math.sin(3.0 * LEIA.time);;
     renderer.setClearColor(new THREE.Color().setRGB(1.0, 1.0, 1.0));
+    renderer.shadowMapEnabled = true;
     renderer.Leia_render({
         scene: scene,
         camera: camera,
@@ -124,93 +86,7 @@ function UpateTimeObject() {
         // console.log("remove");
     }
     bInitTimeObject = true;
-//     var AMPMHH = g_hour;
-//     if (AMPMHH > 12) {
-//         AMPMHH = AMPMHH - 12;
-//     }
-//     if (AMPMHH === 0) {
-//         AMPMHH = 12;
-//     }
-//     var strHour = AMPMHH.toString();
-//     if (AMPMHH < 10)
-//         strHour = "0" + strHour;
-
-//     GeoHH = new THREE.TextGeometry(strHour, {
-//         size: 12,
-//         height: 2,
-//         curveSegments: 4,
-//         font: lf1,
-//         weight: "normal",
-//         style: "normal",
-//         bevelThickness: 0.5,
-//         bevelSize: 0.25,
-//         bevelEnabled: true,
-//         material: 0,
-//         extrudeMaterial: 1
-//     });
-
-//     var strMinutes = g_minute.toString();
-//     if (g_minute < 10)
-//         strMinutes = "0" + strMinutes;
-//     GeoMM = new THREE.TextGeometry(strMinutes, {
-//         size: 6,
-//         height: 2,
-//         curveSegments: 4,
-//         font: lf2,
-//         weight: "normal",
-//         style: "normal",
-//         bevelThickness: 0.5,
-//         bevelSize: 0.25,
-//         bevelEnabled: true,
-//         material: 0,
-//         extrudeMaterial: 1
-//     });
-//     if (g_hour >= 12) {
-//         g_AMPM = "PM";
-//     } else {
-//         g_AMPM = "AM";
-//     }
-//     GeoAM = new THREE.TextGeometry(g_AMPM, {
-//         size: 3,
-//         height: 2,
-//         curveSegments: 4,
-//         font: lf3,
-//         weight: "normal",
-//         style: "normal",
-//         bevelThickness: 0.6,
-//         bevelSize: 0.25,
-//         bevelEnabled: true,
-//         material: 0,
-//         extrudeMaterial: 1
-//     });
-
-//     GeoHH.computeBoundingBox();
-//     GeoHH.computeVertexNormals();
-//     var MeshHH = new THREE.Mesh(GeoHH, material);
-//     GeoMM.computeBoundingBox();
-//     GeoMM.computeVertexNormals();
-//     var MeshMM = new THREE.Mesh(GeoMM, material);
-//     GeoAM.computeBoundingBox();
-//     GeoAM.computeVertexNormals();
-//     var MeshAM = new THREE.Mesh(GeoAM, material);
-//     var HHdx = GeoHH.boundingBox.max.x - GeoHH.boundingBox.min.x;
-//     var HHdy = GeoHH.boundingBox.max.y - GeoHH.boundingBox.min.y;
-//     var MMdx = GeoMM.boundingBox.max.x - GeoMM.boundingBox.min.x;
-//     var MMdy = GeoMM.boundingBox.max.y - GeoMM.boundingBox.min.y;
-//     var AMdx = GeoAM.boundingBox.max.x - GeoAM.boundingBox.min.x;
-//     var AMdy = GeoAM.boundingBox.max.y - GeoAM.boundingBox.min.y;
-//     MeshHH.position.set(-8 - 0.5 * HHdx, -0.5 * HHdy, 0);
-//     MeshMM.position.set(8 - 0.5 * MMdx, -3 - 0.5 * MMdy, 0);
-//     MeshAM.position.set(9 - 0.5 * AMdx, 4 - 0.5 * AMdy, 0);
-//     MeshHH.castShadow = true;
-//     MeshHH.receiveShadow = true;
-//     MeshMM.castShadow = true;
-//     MeshMM.receiveShadow = true;
-//     MeshAM.castShadow = true;
-//     MeshAM.receiveShadow = true;
-    //     group.add(MeshHH);
-    //     group.add(MeshMM);
-    //     group.add(MeshAM);
+  
     var shaderMaterial = new THREE.ShaderMaterial({
 
         uniforms: uniforms,
@@ -218,9 +94,10 @@ function UpateTimeObject() {
         fragmentShader: document.getElementById('sunSurfaceFragmentShader').textContent
 
     });
+  
     var sphere = new THREE.Mesh(new THREE.SphereGeometry(17.5, 100, 100), shaderMaterial);
-//           sphere.overdraw = true;
-
+    sphere.castShadow = true;
+    sphere.receiveShadow = true;
     group.add(sphere);
 
     scene.add(group);
@@ -235,7 +112,6 @@ function addObjectsToScene() {
 function addLights() {
     //Add Lights Here
     var light = new THREE.SpotLight(0xffffff);
-    //light.color.setHSL( Math.random(), 1, 0.5 );
     light.position.set(0, 60, 60);
     light.shadowCameraVisible = false;
     light.castShadow = true;
@@ -257,12 +133,12 @@ function LEIA_setBackgroundPlane(filename, aspect) {
 
     //     //
     //     var planeMaterial = shaderMaterial;
-    //     var planeGeometry = new THREE.PlaneGeometry(80, 60, 10, 10);
-    //     plane = new THREE.Mesh(planeGeometry, planeMaterial);
-    //   console.log(plane);
-    //   plane.updateMatrix();
-    //     plane.position.z = -6;
-    //     plane.castShadow = false;
-    //     plane.receiveShadow = true;
-    //     scene.add(plane);
+        var planeGeometry = new THREE.PlaneGeometry(80, 60, 10, 10);
+        plane = new THREE.Mesh(planeGeometry, new THREE.MeshPhongMaterial);
+      console.log(plane);
+      plane.updateMatrix();
+        plane.position.z = -6;
+        plane.castShadow = false;
+        plane.receiveShadow = true;
+        scene.add(plane);
 }
